d594595f4bf5b4cb0912d7a9a716d7ac65fca529
Rustem Shakirov <oldrefery@gmail.com>
2026-02-24 17:40:05 +0100

    fix: resolve @ path alias in Metro for local EAS iOS bundle

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,18 @@
 const { getDefaultConfig } = require('@expo/metro-config')
 const { getSentryExpoConfig } = require('@sentry/react-native/metro')
+const path = require('path')
 
 // Get default Expo config first
 const defaultConfig = getDefaultConfig(__dirname)
 
 // Apply Sentry modifications to the default config
 const config = getSentryExpoConfig(__dirname, defaultConfig)
+
+// Keep @/ imports stable in local EAS bundle step (expo export:embed).
+config.resolver = config.resolver || {}
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+  '@': path.resolve(__dirname, 'src'),
+}
 
 module.exports = config
